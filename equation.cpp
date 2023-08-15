e44e393f010added80c254f71676b3e5deb3d297
ksshkh <142316121+ksshkh@users.noreply.github.com>
2023-08-15 19:03:48 +0300

    Update equation.cpp

--- equation.cpp
+++ equation.cpp
@@ -3,24 +3,38 @@
 
 int main() {
   double a, b, c, D, x1, x2, x;
-  printf("Введите коэффициенты a, b, c, чтобы решить квадратное уравнение вида\nax^2 + bx + c = 0\n");
+  printf("Р’РІРµРґРёС‚Рµ РєРѕСЌС„С„РёС†РёРµРЅС‚С‹ a, b, c, С‡С‚РѕР±С‹ СЂРµС€РёС‚СЊ РєРІР°РґСЂР°С‚РЅРѕРµ СѓСЂР°РІРЅРµРЅРёРµ РІРёРґР°\nax^2 + bx + c = 0\n");
   printf("a = ");
   scanf("%lf", &a);
   printf("b = ");
   scanf("%lf", &b);
   printf("c = ");
   scanf("%lf", &c);
-  D = b * b - 4 * a * c;
-  if (D > 0) {
-    x1 = (-b + sqrt(D)) / (2 * a);
-    x2 = (-b - sqrt(D)) / (2 * a);
-    printf("x1 = %.2lf\nx2 = %.2lf\n", x1, x2);
+  if (a == 0) {
+    if (b != 0) {
+      x = (-c) / b;
+      printf("x = %.2lf", x);
+    }
+    else {
+      if (c == 0)
+        printf("x - Р»СЋР±РѕРµ С‡РёСЃР»Рѕ.");
+      else
+        printf("Р РµС€РµРЅРёР№ РЅРµС‚.");
+    }
   }
-  else if (D == 0) {
-    x = (-b + sqrt(D)) / (2 * a);
-    printf("x = %.2lf\n", x);
+  else {
+    D = b * b - 4 * a * c;
+    if (D > 0) {
+      x1 = (-b + sqrt(D)) / (2 * a);
+      x2 = (-b - sqrt(D)) / (2 * a);
+      printf("x1 = %.2lf\nx2 = %.2lf\n", x1, x2);
+    }
+    else if (D == 0) {
+      x = (-b + sqrt(D)) / (2 * a);
+      printf("x = %.2lf\n", x);
+    }
+    else
+      printf("Р РµС€РµРЅРёР№ РЅРµ СЃСѓС‰РµСЃС‚РІСѓРµС‚.\n");
   }
-  else
-    printf("Решений не существует.\n");
   return 0;
 }
