cc7c6df8336c166c6933ea8658f15ccdf6e9df4a
ksshkh <142316121+ksshkh@users.noreply.github.com>
2023-08-17 19:27:03 +0300

    последнее

--- equationv2.cpp
+++ equationv2.cpp
@@ -3,82 +3,92 @@
 
 #define EPS 1e-9
 
+enum solutions {
+  NO_ROOTS,
+  SINGLE_ROOT,
+  TWO_ROOTS,
+  INFINITY_ROOTS
+};
+
 bool compare(double x, double y);
-double discriminant(double a, double b, double c);
-double root1(double b, double D, double a);
-double root2(double b, double D, double a);
-double input(void);
+int root_finder(double a, double b, double c, double * x1, double * x2);
+double input(char letter);
 
 int main() {
 
   double a = 0;
   double b = 0;
   double c = 0;
-  double D = 0;
   double x1 = 0;
   double x2 = 0;
 
   printf("Введите коэффициенты a, b, c, чтобы решить квадратное уравнение вида\nax^2 + bx + c = 0\n");
-  printf("a = ");
-  a = input();
-  printf("b = ");
-  b = input();
-  printf("c = ");
-  c = input();
+  a = input('a');
+  b = input('b');
+  c = input('c');
 
-  if (compare(a, 0)) {
-    if (!compare(b, 0)) {
-      x1 = (-c) / b;
-      printf("x = %.2lf", x1);
-    }
-    else {
-      if (compare(c, 0)) {
-        printf("x - любое число.");
-      }
-      else {
-        printf("Решений нет.");
-      }
-    }
+  switch (root_finder(a, b, c, &x1, &x2)) {
+    case NO_ROOTS:
+      printf("Решений нет.");
+      break;
+    case SINGLE_ROOT:
+      printf("x = %.5lf", x1);
+      break;
+    case TWO_ROOTS:
+      printf("x1 = %.5lf\nx2 = %.5lf", x1, x2);
+      break;
+    default:
+      printf("x - любое число.");
+      break;
   }
-  else {
-    D = discriminant(a, b, c);
-    if (D > 0) {
-      x1 = root1(b, D, a);
-      x2 = root2(b, D, a);
-      printf("x1 = %.2lf\nx2 = %.2lf\n", x1, x2);
-    }
-    else if (compare(D, 0)) {
-      x1 = root1(b, D, a);
-      printf("x = %.2lf\n", x1);
-    }
-    else {
-      printf("Решений не существует.\n");
-    }
-  }
+
   return 0;
-}
-
-double discriminant(double a, double b, double c) {
-  return b * b - 4 * a * c;
 }
 
 bool compare(double x, double y) {
   return fabs(x - y) < EPS;
 }
 
-double root1(double b, double D, double a) {
-  return (-b + sqrt(D)) / (2 * a);
-}
-
-double root2(double b, double D, double a) {
-  return (-b - sqrt(D)) / (2 * a);
-}
-
-double input(void) {
+double input(char letter) {
   double koef;
-  while (!scanf("%lf", &koef)) {
+  printf("%c = ", letter);
+  while (scanf("%lf", &koef) != 1 && koef != EOF) {
     scanf("%*s");
     printf("Введите число\n");
+    printf("%c = ", letter);
   }
   return koef;
+}
+
+int root_finder(double a, double b, double c, double * x1, double * x2) {
+  double D = 0;
+  if (compare(a, 0)) {
+    if (!compare(b, 0)) {
+      *x1 = (-c) / b;
+      return SINGLE_ROOT;
+    }
+    else {
+      if (compare(c, 0)) {
+        return INFINITY_ROOTS;
+      }
+      else {
+        return NO_ROOTS;
+      }
+    }
+  }
+  else {
+    D = b * b - 4 * a * c;
+    if (D > 0) {
+      *x1 = (-b + sqrt(D)) / (2 * a);
+      *x2 = (-b - sqrt(D)) / (2 * a);
+      return TWO_ROOTS;
+    }
+    else if (compare(D, 0)) {
+        *x1 = (-b + sqrt(D)) / (2 * a);
+        return SINGLE_ROOT;
+      }
+    else {
+        return NO_ROOTS;
+      }
+  }
 }
